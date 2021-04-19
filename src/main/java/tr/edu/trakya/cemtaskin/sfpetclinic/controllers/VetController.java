496f8877006ffb39b6d52d2a4989528c716570f9
Cem TASKIN <cemtaskin82@gmail.com>
2021-04-19 10:55:29 +0300

    vets listed on index page. closes #9

--- src/main/java/tr/edu/trakya/cemtaskin/sfpetclinic/controllers/VetController.java
+++ src/main/java/tr/edu/trakya/cemtaskin/sfpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package tr.edu.trakya.cemtaskin.sfpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tr.edu.trakya.cemtaskin.sfpetclinic.services.map.VetServiceMap;
 
 @Controller
 public class VetController {
 
+    private final VetServiceMap vetServiceMap;
+
+    public VetController(VetServiceMap vetServiceMap) {
+        this.vetServiceMap = vetServiceMap;
+    }
+
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets",vetServiceMap.findAll());
         return "vets/index";
     }
 }
